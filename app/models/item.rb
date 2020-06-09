917c660d0b8eae7a8be0bdd6b32c2fde1ce1c972
ks0112 <k.sugihara.12@gmail.com>
2020-06-09 12:04:45 +0900

    header,footerの部分テンプレート切り分け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :seller, class_name: “user”, foreign_key: “seller_id”
-  belongs_to :buyer, class_name: “user”, foreign_key: “buyer_id”
+  belongs_to :seller, class_name: "User", foreign_key: "seller_id"
+  belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
   belongs_to :category, dependent: :destroy
   belongs_to :brand, dependent: :destroy
   has_many :images, dependent: :destroy
