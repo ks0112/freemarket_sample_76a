0738f2f06ad707022ee053df9571422a21e6927f
sabosaboten <deyakei@gmail.com>
2020-06-10 17:35:35 +0900

    回送表示追加基礎完成

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -96,24 +96,46 @@ $(document).on('turbolinks:load', function(){
 
 
 
+// $(document).ready(function(){
+
+//   $('.parent').hover(function(){
+//     $(this).addClass('active');
+//     var children = $('.active').children('.child-wrap');
+//     children.show();
+//   },function(){
+//     $(this).removeClass('active');
+//     $(this).children('.child-wrap').hide();
+//   });
+
+//   $('.child').hover(function(){
+//     $(this).addClass('active2');
+//     var grandChild = $('.active2').children('.grand-child-wrap');
+//     grandChild.show();
+//   },function(){
+//     $(this).removeClass('active2');
+//     $(this).children('.grand-child-wrap').hide();
+
+//   });
+// });
+
 $(document).ready(function(){
 
-  $('.parent').hover(function(){
+  $('.category').hover(function(){
     $(this).addClass('active');
-    var children = $('.active').children('.child-wrap');
+    var children = $('.active').children('.parent-wrap');
     children.show();
   },function(){
     $(this).removeClass('active');
-    $(this).children('.child-wrap').hide();
+    $(this).children('.parent-wrap').hide();
   });
 
-  $('.child').hover(function(){
+  $('.parent').hover(function(){
     $(this).addClass('active2');
-    var grandChild = $('.active2').children('.grand-child-wrap');
+    var grandChild = $('.active2').children('.child-wrap');
     grandChild.show();
   },function(){
     $(this).removeClass('active2');
-    $(this).children('.grand-child-wrap').hide();
+    $(this).children('.child-wrap').hide();
 
   });
 });
