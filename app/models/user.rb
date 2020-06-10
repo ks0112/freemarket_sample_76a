a2cad04a979e8d83e9a1bba116489a7777b305a7
ks0112 <k.sugihara.12@gmail.com>
2020-06-10 09:19:02 +0900

    usersテーブル更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :destinations, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :items, dependent: :destroy
-  # has_many :selling_items, class_name: "Item", foreign_key: "seller_id", -> { where("buyer_id is NULL") }
+  has_many :selling_items, -> { where("seller_id is not NULL && buyer_id is NULL") }, class_name: "Item"
   has_many :bought_items, class_name: "Item", foreign_key: "buyer_id"
-  # has_many :sold_items, class_name: "Item", foreign_key: "seller_id", -> { where("buyer_id is not NULL") }
+  has_many :sold_items, -> { where("buyer_id is not NULL") }, class_name: "Item"
 end
