7d4088a505e4f75993831f6e349e9dadc89654b2
kazu09 <kita.k50902@gmail.com>
2020-06-15 20:22:42 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_current_user_items,only:[:p_exhibiting,:p_soldout]
   before_action :set_user,only:[:p_exhibiting,:p_soldout]
-  before_action :set_item, only:[:show, :destroy, :edit, :update, :purchase, :payment]
+  before_action :set_item, only:[:show, :destroy, :update, :purchase, :payment]
 
 
   def index
@@ -36,10 +36,38 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def edit
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_update_params)
+    # @images = Image.where(item_id: @item.id)
+    # image_form_count = item_params_update[:item_images_attributes].to_hash.length
+    # image_count = @images.length
+    # delete_image_count = 0
+    # count = 0
+    # while count < image_count do
+    #   if item_params_update[:item_images_attributes].values[count][:"_destroy"] == "1"
+    #     delete_image_count += 1
+    #   end
+    #   count += 1
+    # end
+    # if delete_image_count != image_count || image_form_count != image_count
+    #   @item.update(item_params_update)
+    #   redirect_to root_path
+    # else
+    #   redirect_to edit_item_path(params[:id])
+    # end
+
+    product = Item.find(params[:id])
+    if product.user_id == current_user.id
+      product.update(product_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
-  def update
+  def edit
+    @item = Item.find(params[:id])
   end
 
   def destroy
@@ -85,5 +113,11 @@ class ItemsController < ApplicationController
 
   def set_user
     @user = User.find(current_user.id)
+  end
+  # 画像
+  def item_update_params
+    params.require(:item).permit(
+      :name,
+      [images_attributes: [:image, :_destroy, :id]])
   end
 end
