e3a11f95172244c6a9cca11b4f5e8fe8872b1207
ks0112 <k.sugihara.12@gmail.com>
2020-06-15 10:56:07 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
@@ -11,11 +16,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
-    storage :fog
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
