154adbce90425882d86c4e20b7725e4672e8cff6
ks0112 <k.sugihara.12@gmail.com>
2020-06-15 19:53:32 +0900

    add

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,13 +2,24 @@ class DestinationsController < ApplicationController
   def index
   end
 
-  def show
+
+  def new
+    destination = Destination.where(user_id: current_user.id)
+    redirect_to edit_destination_path(current_user.id) if destination.exists?
+    @destination = Destination.new
   end
 
+  def create
+    @destination = Destination.create(destination_params)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def show
+  end
   def edit
     destination = Destination.find_by(user_id: current_user.id)
     if destination.blank?
-      redirect_to new_destination_path 
+      redirect_to new_destination_path
     else
     @destination = Destination.find(params[:id])
     end
@@ -19,18 +30,6 @@ class DestinationsController < ApplicationController
     @destination.update(destination_params)
     redirect_to edit_destination_path(current_user.id)
   end
-
-  def new
-    destination = Destination.where(user_id: current_user.id)
-    redirect_to edit_destination_path(current_user.id) if destination.exists?
-    @destination = Destination.new
-  end
-
-  def create
-    @destination = Destination.create(destination_params)
-    redirect_to edit_destination_path(current_user.id)
-  end
-
   private
   def destination_params
     params.require(:destination).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
