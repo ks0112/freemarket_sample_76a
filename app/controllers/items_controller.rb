fb312c9d7c2d94655b02490b4454a67b1350cbce
sabosaboten <deyakei@gmail.com>
2020-06-15 17:01:43 +0900

    item-edit追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :set_current_user_items,only:[:p_exhibiting,:p_soldout]
   before_action :set_user,only:[:p_exhibiting,:p_soldout]
-  before_action :set_item, only:[:show, :destroy, :edit, :update, :purchase, :payment]
-
+  # before_action :set_item, only:[:show, :destroy, :edit, :update, :purchase, :payment]
+  before_action :set_item, only:[:show, :destroy, :update, :purchase, :payment]
 
   def index
     @item = Item.limit(3).order('created_at DESC')
@@ -37,6 +37,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    # 親セレクトボックスの初期値(配列)
+    @category_parent_array = []
+    # categoriesテーブルから親カテゴリーのみを抽出、配列に格納
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    # itemに紐づいていいる孫カテゴリーの親である子カテゴリが属している子カテゴリーの一覧を配列で取得
+    @category_child_array = @item.category.parent.parent.children
+
+    # itemに紐づいていいる孫カテゴリーが属している孫カテゴリーの一覧を配列で取得
+    @category_grandchild_array = @item.category.parent.children
   end
 
   def update
