728c93c8ab938180aeb980db2483c09ff0fdd305
ks0112 <k.sugihara.12@gmail.com>
2020-06-15 16:23:16 +0900

    add

--- db/migrate/20200615065957_change_datatype_category_id_of_items.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDatatypeCategoryIdOfItems < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :items, :category_id, :category
-    change_column :items, :category, :references, :null: false, :foreign_key: true
-  end
-end
