6eaf275709a08afc4a130c03be063155c68f4034
ks0112 <k.sugihara.12@gmail.com>
2020-06-16 17:42:40 +0900

    fix uploader

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# set :linked_files, %w{ config/master.key }
-append :linked_files, 'config/master.key'
+set :linked_files, %w{ config/master.key }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
