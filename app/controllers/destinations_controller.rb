b9549ee07254281cb4e69a4ea49600fafbdd3e47
sabosaboten <deyakei@gmail.com>
2020-06-16 23:25:26 +0900

    destination修正

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,10 +3,19 @@ class DestinationsController < ApplicationController
   end
 
 
+  # def new
+  #   destination = Destination.where(user_id: current_user.id)
+  #   redirect_to edit_destination_path(current_user.id) if destination.exists?
+  #   @destination = Destination.new
+  # end
+
   def new
-    destination = Destination.where(user_id: current_user.id)
-    redirect_to edit_destination_path(current_user.id) if destination.exists?
+    destination = Destination.find_by(user_id: current_user.id)
+    unless destination.blank?
+      redirect_to edit_destination_path(destination.id)
+    else
     @destination = Destination.new
+    end
   end
 
   def create
@@ -16,20 +25,30 @@ class DestinationsController < ApplicationController
 
   def show
   end
+  # def edit
+  #   destination = Destination.find_by(user_id: current_user.id)
+  #   if destination.blank?
+  #     redirect_to new_destination_path
+  #   else
+  #   @destination = Destination.find(params[:id])
+  #   end
+  # end
   def edit
     destination = Destination.find_by(user_id: current_user.id)
-    if destination.blank?
-      redirect_to new_destination_path
-    else
-    @destination = Destination.find(params[:id])
-    end
   end
+
+  # def update
+  #   @destination = Destination.find(params[:id])
+  #   @destination.update(destination_params)
+  #   redirect_to edit_destination_path(current_user.id)
+  # end
 
   def update
     @destination = Destination.find(params[:id])
     @destination.update(destination_params)
     redirect_to edit_destination_path(current_user.id)
   end
+
   private
   def destination_params
     params.require(:destination).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,9 +2,23 @@ class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
+    # if card.blank?
+    #   #登録された情報がない場合にカード登録画面に移動
+    #   redirect_to new_card_path
+    # else
+    # Payjp.api_key = "sk_test_6d5bbe82c50db611a63aeefa"
+    # #保管した顧客IDでpayjpから情報取得
+    # customer = Payjp::Customer.retrieve(card.customer_id)
+    # #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+    # @default_card_information = customer.cards.retrieve(card.card_id)
+    # end
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to new_card_path
+    elsif current_user.destinations.blank? then
+      redirect_to new_destination_path
+    elsif @item.buyer_id || @item.id.blank then
+      redirect_to root_path
     else
     Payjp.api_key = "sk_test_6d5bbe82c50db611a63aeefa"
     #保管した顧客IDでpayjpから情報取得
