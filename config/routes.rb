6d3725e559cb81d7c8c962b12a0548c655aa494b
sabosaboten <deyakei@gmail.com>
2020-06-21 09:17:56 +0900

    Revert "Merge branch 'master' into 売り編集機能"
    This reverts commit 74338414d7f46163e0c538570c9d7276558467bf, reversing
    changes made to 6804f3fe888fea8ac31c5c8c0bb6c2ac3a5ae034.

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,12 @@ Rails.application.routes.draw do
   resources :users, only: [:show,:index]
   resources :images
   resources :destinations
-  resources :purchases
   resources :cards, only: [:new, :show, :destroy] do
     collection do
       post 'pay', to: 'cards#pay'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :items do
-  #   #Ajaxで動くアクションのルートを作成
-  #   collection do
-  #     get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
-  #     get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
-  #   end
-  #   member do
-  #     post 'buy', to: 'items#buy'
-  #     get 'done', to: 'items#done'
-  #   end
-  # end
   resources :items do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
@@ -32,12 +20,5 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
-    resources :purchases, only: [:index] do
-      collection do
-        get 'done', to: 'purchases#done'
-        post 'buy', to: 'purchases#buy'
-      end
-    end
   end
-
 end
--- db/migrate/20200605064457_create_items.rb
+++ db/migrate/20200605064457_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.string :description, null: false
       t.integer :price, null: false
-      t.references :category, null: false, foreign_key: true
+      t.integer :category_id, null: false, foreign_key: true
       t.integer :brand_id, foreign_key: true
       t.integer :status_id, null: false
       t.integer :cost_id, null: false
